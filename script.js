42614b215904eab6d90b240a3d9d7526067e594e
Corbin Cutter <Corbin@Lisas-iMac.lan>
2022-11-09 00:41:33 -0500

    Working on API functionality

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 var apiKey = "1edc84713465d4aba9696522d1dc5fc4";
-var apiUrl = "https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}"
+// var apiUrl = "https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}";
 // var apiKey = "1edc84713465d4aba9696522d1dc5fc4";
-// var currentDay = moment().format('ll');
+var currentDay = moment().format("MMM Do YY");  
 var searchHistory = [];
 
 // Function to display the current conditions of the city that was searched for
+
 function searchedCity(city) {
+
+    var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;;
 
 $.ajax({
     url: apiUrl,
@@ -20,64 +23,50 @@ $.ajax({
 
     var currentCity = $(`
         <h2 id="currentCity">
-            ${cityWeatherResponse.name} ${today} <img src="${iconURL}" alt="${cityWeatherResponse.weather[0].description}" />
+            ${weatherResponse.name} ${currentDay} <img src="${iconURL}" alt="${weatherResponse.weather[0].description}" />
         </h2>
-        <p>Temperature: ${cityWeatherResponse.main.temp} °F</p>
-        <p>Humidity: ${cityWeatherResponse.main.humidity}\%</p>
-        <p>Wind Speed: ${cityWeatherResponse.wind.speed} MPH</p>
+        <p>Temperature: ${weatherResponse.main.temp} °F</p>
+        <p>Humidity: ${weatherResponse.main.humidity}\%</p>
+        <p>Wind Speed: ${weatherResponse.wind.speed} MPH</p>
     `);
 
     $("#current-city-conditions").append(currentCity);
+    var lat = weatherResponse.coord.lat;
+    var lon = weatherResponse.coord.lon;
+    fiveDayForecast(lat, lon);
   });
 }
-
-$("#citySearchButton").on("click", function(event) {
-    event.preventDefault();
-
-    var city = $("#city-input-id").val().trim();
-    searchedCity(city);
-    if (!searchHistoryList.includes(city)) {
-        searchHistoryList.push(city);
-        var searchedCity = $(`
-            <li class="list-group-item">${city}</li>
-            `);
-        $("#list-of-cities").append(searchedCity);
-    };
-    
-    localStorage.setItem("city", JSON.stringify(searchHistory));
-    console.log(searchHistory);
-});
 
 // Function to display weather for city over the next 5 days
 
 function fiveDayForecast(lat, lon) {
 
-    var fiveDayUrl = `https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}`;
+    var fiveDayUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
 
     $.ajax({
         url: fiveDayUrl,
         method: 'GET',
-      }).then(function (weatherResponse) {
-        $("#weatherContent").css("display", "block");
-        $("#current-city-conditions").empty();
+      }).then(function (futureResponse) {
+        console.log(futureResponse);
+        $("#fiveDay").empty();
 
-        for (let i = 1; i < 6; i++) {
+        for (let i = 9; i < 40; i+= 8) {
             var fiveDayInfo = {
-                date: futureResponse.daily[i].dt,
-                icon: futureResponse.daily[i].weather[0].icon,
-                temp: futureResponse.daily[i].temp.day,
-                humidity: futureResponse.daily[i].humidity
+                date: futureResponse.list[i].dt,
+                icon: futureResponse.list[i].weather[0].icon,
+                temp: futureResponse.list[i].main.temp,
+                humidity: futureResponse.list[i].main.humidity,
             };
 
-            var currDate = moment.unix(cityInfo.date).format("MM/DD/YYYY");
-            var iconURL = `<img src="https://openweathermap.org/img/w/${cityInfo.icon}.png" alt="${futureResponse.daily[i].weather[0].main}" />`;
+            var currDate = moment.unix(fiveDayInfo.date).format("MM/DD/YYYY");
+            var iconURL = `<img src="https://openweathermap.org/img/w/${fiveDayInfo.icon}.png" alt="${futureResponse.list[i].weather[0].icon}" />`;
 
             var fiveDayForecastCard = $(`
                         <div class="card-body">
                             <h5>${currDate}</h5>
                             <p>${iconURL}</p>
-                            <p>Temp: ${cityInfo.temp} °F</p>
-                            <p>Humidity: ${cityInfo.humidity}\%</p>
+                            <p>Temp: ${fiveDayInfo.temp} °F</p>
+                            <p>Humidity: ${fiveDayInfo.humidity}\%</p>
                         </div>
             `);
 
@@ -86,22 +75,23 @@ function fiveDayForecast(lat, lon) {
     }); 
 }
 
-var citySearchButtonEl = document.querySelector("#citySearchButton");
+    var citySearchButtonEl = document.querySelector("#citySearchButton");
 
-    citySearchButtonEl.addEventListener('click', function() {
-        console.log('Hello World');
-// citySearchButtonEl.addEventListener("click", function() {
-    // event.preventDefault();
+    citySearchButtonEl.addEventListener("click", function(event) {
+
     console.log('Hello World');
+    event.preventDefault();
+    
     var city = $("#city-input-id").val().trim();
     searchedCity(city);
+    // fiveDayForecast(lat, lon);
     
     if (!searchHistory.includes(city)) {
         searchHistory.push(city);
-        var searchedCity = $(`
+        var currCity = $(`
             <li id="city-search-history">${city}</li>
             `);
-        $("#searchHistory").append(searchedCity);
+        $("#searchHistory").append(currCity);
     }
 
     localStorage.setItem("city", JSON.stringify(searchHistory));
@@ -109,9 +99,11 @@ var citySearchButtonEl = document.querySelector("#citySearchButton");
 });
 
 
-var citySearchHistoryEl = document.getElementById("#city-search-history");
+    var citySearchHistoryEl = document.querySelector("#city-search-history");
 
-citySearchHistoryEl.addEventListener("click", function() {
+    citySearchHistoryEl.addEventListener("click", function(event) {
+    event.preventDefault();
+
     var listCity = $(this).text();
     searchedCity(listCity);
 });
